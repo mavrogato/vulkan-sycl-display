5c11824719a9c3c1f66874160a309a12567fa3ee
Nakashima, Terumi <nakashima.terumi@gmail.com>
2025-11-15 00:51:24 +0900

    Add tuple stream insertion operator support in aux
    Introduces the `aux::tuple_like` concept and a stream insertion operator (`operator<<`) that allows printing any tuple-like type to an output stream.
    A corresponding test case using Catch2 is also included in the 'aux' directory.

---
+++ aux/tuple-support.hpp
@@ -0,0 +1,38 @@
+#ifndef INCLUDE_AUX_TUPLE_SUPPORT_HPP
+#define INCLUDE_AUX_TUPLE_SUPPORT_HPP
+
+#include <tuple>
+#include <concepts>
+#include <utility>
+
+namespace aux
+{
+    template <class T, size_t I>
+    concept has_tuple_element = requires(T t) {
+        typename std::tuple_element_t<I, std::remove_const_t<T>>;
+        { get<I>(t) } -> std::convertible_to<std::tuple_element_t<I, T> const&>;
+    };
+    template <class T>
+    concept tuple_like = !std::is_reference_v<T> && requires(T) {
+        std::tuple_size<T>::value;
+        //requires std::derived_from<std::tuple_size<T>, std::integral_constant<size_t, std::tuple_size_v<T>>>;
+    } && []<size_t... I>(std::index_sequence<I...>) noexcept {
+        return (has_tuple_element<T, I>&& ...);
+    }(std::make_index_sequence<std::tuple_size_v<T>>());
+
+    template <class Ch, class Tr, aux::tuple_like T>
+    auto& operator<<(std::basic_ostream<Ch, Tr>& output, T const& t) noexcept {
+        output.put('(');
+        [&]<size_t ...I>(std::index_sequence<I...>) noexcept {
+            (void) (int[]) {(output << (I==0 ? "" : " ") << get<I>(t), 0)...};
+        }(std::make_index_sequence<std::tuple_size_v<T>>());
+        output.put(')');
+        return output;
+    }
+} // ::aux
+
+namespace std
+{
+} // ::std
+
+#endif // INCLUDE_AUX_TUPLE_SUPPORT_HPP
